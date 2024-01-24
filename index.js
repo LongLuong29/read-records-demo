9d6d8561029dba148517a87e27a49ec844976b61
LongLuong29 <long290901@gmail.com>
2024-01-24 11:39:07 +0700

    fixed update, delete record

--- index.js
+++ index.js
@@ -6,57 +6,8 @@ var totalRecord = myData.length;
 var totalPages = Math.ceil(totalRecord / recordPerPage);
 
 loadData();
-// displayCurrentPage();
-createPaginationButtons();
 
-$(".pagination").on('click', function(e){
-    e.preventDefault();
-    currentPage = parseInt($(this).text());
-    displayCurrentPage();
-})
-
-function displayCurrentPage(){
-    var startIndex = (currentPage - 1) * recordPerPage;
-    var endIndex = startIndex + recordPerPage;
-    var currentRecord = myData.slice(startIndex, endIndex);
-
-    var html = "";
-
-    $.each(currentRecord, function(index, item){
-        html += "<tr>";
-        html += `<td><input class="recordCheckbox" value="${myData.indexOf(item)}" type="checkbox"/></td>`;
-        html += "<td>" + (index + 1) + "</td>";
-        html += "<td>" + item.name + "</td>";
-        html += "<td>" + item.description + "</td>";
-        html += "<td>" + item.price + "</td>";
-        html += "<td>" + item.category + "</td>";
-        html += "<td class='hidden'>" + myData.indexOf(item) + "</td>";
-        html += `<td>
-                    <button type="button" class="recordEdit btn btn-info" 
-                    data-toggle="modal" 
-                    data-target="#updateModal">
-                        <span class="glyphicon glyphicon-edit"></span> Edit
-                    </button>`;
-        html += `   <button type="button" class="deleteRecord btn btn-danger" 
-                    value="${myData.indexOf(item)}">
-                        <span class="glyphicon glyphicon-trash"></span> Delete
-                    </button>
-                </td>`;
-        html += "</tr>";
-    })
-
-    $("#myTable").html(html);
-}
-
-function createPaginationButtons() {
-    var paginationHtml = "";
-    for (var i = 1; i <= totalPages; i++) {
-        paginationHtml += 
-        `<li class="page-item ${i == currentPage? "active" : ""}"><a class="page-link" href="#">${i}</a></li>`;
-    }
-    $("ul.pagination").html(paginationHtml);
-}
-
+//searching
 $("input#searchingInput").on("keyup", function () {
     var inputValue = $(this).val();
     var filteredData = myData.filter(function (item) {
@@ -64,12 +15,11 @@ $("input#searchingInput").on("keyup", function () {
     })  
     var html = "";
     filteredData.forEach(function (item, index) {
-        html += "<tr>";
+        html += `<tr class='deletedCheckbox' value="${myData.indexOf(item)}">`;
         html += `<td><input class="recordCheckbox" value="${myData.indexOf(item)}" type="checkbox"/></td>`;
-        html += "<td>" + (index + 1) + "</td>";
         html += "<td>" + item.name + "</td>";
         html += "<td>" + item.description + "</td>";
-        html += "<td>" + item.price + "</td>";
+        html += "<td>$" + item.price + "</td>";
         html += "<td>" + item.category + "</td>";
         html += "<td class='hidden'>" + myData.indexOf(item) + "</td>";
         html += `<td>
@@ -79,7 +29,7 @@ $("input#searchingInput").on("keyup", function () {
                         <span class="glyphicon glyphicon-edit"></span> Edit
                     </button>`;
         html += `   <button type="button" class="deleteRecord btn btn-danger" 
-                    value="${myData.indexOf(item)}">
+                    value="${item.name}">
                         <span class="glyphicon glyphicon-trash"></span> Delete
                     </button>
                 </td>`;
@@ -88,12 +38,22 @@ $("input#searchingInput").on("keyup", function () {
     $("tbody").html(html);
 })
 
-$(".saveEdit").on("click", function () {
+//edit - fixed
+$(document).on("click",".saveEdit", function(){
     var nameValue = $("#nameEdit").val();
     var descriptionValue = $("#descriptionEdit").val();
     var priceValue = $("#priceEdit").val();
     var categoryValue = $("#categoryEdit").val();
     var recordIndexValue = $("#recordIndex").val();
+
+    var currentRow = $("tr[value='"+recordIndexValue+"']")
+    currentRow.find("td:nth-child(1)").val(recordIndexValue)
+    currentRow.find("td:nth-child(2)").text(nameValue)
+    currentRow.find("td:nth-child(3)").text(descriptionValue);
+    currentRow.find("td:nth-child(4)").text("$"+priceValue) 
+    currentRow.find("td:nth-child(5)").text(categoryValue);
+    currentRow.find("td:nth-child(6)").text(recordIndexValue);
+    currentRow.find("button:last-child").val(nameValue)
 
     var record = {
         name: nameValue,
@@ -105,46 +65,10 @@ $(".saveEdit").on("click", function () {
     var jsonString = JSON.stringify(myData);
     //Lưu dữ liệu vào localStorage
     localStorage.setItem("data", jsonString);
-    location.reload();
 })
 
-
-
-
-function loadData() {
-    var jsonString = localStorage.getItem('data');
-    if (jsonString) {
-        myData = JSON.parse(jsonString);
-        for (const records of myData) {
-            var innerHTML = `
-        <tr class="deletedCheckbox" value="${myData.indexOf(records)}">
-            <td><input class="recordCheckbox" value="${myData.indexOf(records)}" type="checkbox"/></td>
-            <td>${records.name}</td>
-            <td>${records.description}</td>
-            <td>$${records.price}</td> 
-            <td>${records.category}</td>
-            <td class="hidden">${myData.indexOf(records)}</td>
-            <td>
-                <button type="button" class="recordEdit btn btn-info" 
-                data-toggle="modal" 
-                data-target="#updateModal">
-                    <span class="glyphicon glyphicon-edit"></span> Edit
-                </button>
-                <button type="button" class="deleteRecord btn btn-danger" 
-                value="${myData.indexOf(records)}">
-                    <span class="glyphicon glyphicon-trash"></span> Delete
-                </button>
-            </td>
-        </tr>
-        `;
-            // data-toggle="modal" 
-            // data-target="#deleteRecordModal"
-            $("#myTable").append(innerHTML)
-        }
-    }
-}
-
-$(".recordEdit").on("click", function () {
+//lay thong tin record len edit
+$(document).on("click", ".recordEdit", function () {
     //Lay thong tin
     var selectedRecord = $(this).closest("tr");
     var name = selectedRecord.find("td:nth-child(2)").text()
@@ -156,22 +80,35 @@ $(".recordEdit").on("click", function () {
     //Hien thi
     $("#nameEdit").val(name)
     $("#descriptionEdit").val(description)
-    $("#priceEdit").val(price)
+    $("#priceEdit").val(price.split("$")[1])
     $("#categoryEdit").val(category)
     $("#recordIndex").val(index)
 
 })
 
-$("button.deleteRecord").on("click", function () {
+//delete fixed
+$(document).on("click", "button.deleteRecord", function () {
     var clickedButton = $(this);
-    myData.splice(clickedButton.val(), 1)
+    // myData.splice(clickedButton.val(), 1)
+    console.log(clickedButton.val())
+    console.log(clickedButton.closest("tr"));
+    console.log(myData)
+    deleteElementByName(myData,clickedButton.val())
+
     var jsonString = JSON.stringify(myData);
     //Lưu dữ liệu vào localStorage
     localStorage.setItem("data", jsonString);
-    location.reload();
     clickedButton.closest("tr").remove();
 })
 
+function deleteElementByName(arr, nameToDelete) {
+    const indexToDelete = arr.findIndex(item => item.name === nameToDelete);
+    if (indexToDelete !== -1) {
+        arr.splice(indexToDelete, 1);
+    }
+}
+
+//save data - fixed
 function saveData() {
     var nameValue = $("#nameInput").val();
     var descriptionValue = $("#descriptionInput").val();
@@ -191,7 +128,27 @@ function saveData() {
 
     //Lưu dữ liệu vào localStorage
     localStorage.setItem("data", jsonString);
-    location.reload();
+    var innerHTML = `
+        <tr class="deletedCheckbox" value="${myData.indexOf(userData)}">
+            <td><input class="recordCheckbox" value="${myData.indexOf(userData)}" type="checkbox"/></td>
+            <td>${userData.name}</td>
+            <td>${userData.description}</td>
+            <td>$${userData.price}</td> 
+            <td>${userData.category}</td>
+            <td class="hidden">${myData.indexOf(userData)}</td>
+            <td>
+                <button type="button" class="recordEdit btn btn-info" 
+                data-toggle="modal" 
+                data-target="#updateModal">
+                    <span class="glyphicon glyphicon-edit"></span> Edit
+                </button>
+                <button type="button" class="deleteRecord btn btn-danger" 
+                value="${userData.name}">
+                    <span class="glyphicon glyphicon-trash"></span> Delete
+                </button>
+            </td>
+        </tr>`;
+    $("#myTable").append(innerHTML)
 }
 
 function selectAllRecords() {
@@ -214,6 +171,7 @@ function deleteSelected() {
 
     selectedCheckboxes.each(function (index, element) {
         indexes.push(element.value);
+        selectedCheckboxes.closest("tr").remove();
     })
 
     var newMyData = [];
@@ -227,6 +185,91 @@ function deleteSelected() {
     //Lưu dữ liệu vào localStorage
     var jsonString = JSON.stringify(myData);
     localStorage.setItem("data", jsonString);
-    location.reload();
 }
+
+function loadData() {
+    var jsonString = localStorage.getItem('data');
+    if (jsonString) {
+        myData = JSON.parse(jsonString);
+        for (const records of myData) {
+            var innerHTML = `
+            <tr class="deletedCheckbox" value="${myData.indexOf(records)}">
+                <td><input class="recordCheckbox" value="${myData.indexOf(records)}" type="checkbox"/></td>
+                <td>${records.name}</td>
+                <td>${records.description}</td>
+                <td>$${records.price}</td> 
+                <td>${records.category}</td>
+                <td class="hidden">${myData.indexOf(records)}</td>
+                <td>
+                    <button type="button" class="recordEdit btn btn-info" 
+                    data-toggle="modal" 
+                    data-target="#updateModal">
+                        <span class="glyphicon glyphicon-edit"></span> Edit
+                    </button>
+                    <button type="button" class="deleteRecord btn btn-danger" 
+                    value="${records.name}">
+                        <span class="glyphicon glyphicon-trash"></span> Delete
+                    </button>
+                </td>
+            </tr> `;
+            // data-toggle="modal" 
+            // data-target="#deleteRecordModal"
+            $("#myTable").append(innerHTML)
+        }
+    }
+}
+
+
+
+// displayCurrentPage();
+// createPaginationButtons();
+
+// $(".pagination").on('click', function(e){
+//     e.preventDefault();
+//     currentPage = parseInt($(this).text());
+//     displayCurrentPage();
+// })
+
+// function displayCurrentPage(){
+//     var startIndex = (currentPage - 1) * recordPerPage;
+//     var endIndex = startIndex + recordPerPage;
+//     var currentRecord = myData.slice(startIndex, endIndex);
+
+//     var html = "";
+
+//     $.each(currentRecord, function(index, item){
+//         html += "<tr>";
+//         html += `<td><input class="recordCheckbox" value="${myData.indexOf(item)}" type="checkbox"/></td>`;
+//         html += "<td>" + (index + 1) + "</td>";
+//         html += "<td>" + item.name + "</td>";
+//         html += "<td>" + item.description + "</td>";
+//         html += "<td>" + item.price + "</td>";
+//         html += "<td>" + item.category + "</td>";
+//         html += "<td class='hidden'>" + myData.indexOf(item) + "</td>";
+//         html += `<td>
+//                     <button type="button" class="recordEdit btn btn-info" 
+//                     data-toggle="modal" 
+//                     data-target="#updateModal">
+//                         <span class="glyphicon glyphicon-edit"></span> Edit
+//                     </button>`;
+//         html += `   <button type="button" class="deleteRecord btn btn-danger" 
+//                     value="${myData.indexOf(item)}">
+//                         <span class="glyphicon glyphicon-trash"></span> Delete
+//                     </button>
+//                 </td>`;
+//         html += "</tr>";
+//     })
+
+//     $("#myTable").html(html);
+// }
+
+// function createPaginationButtons() {
+//     var paginationHtml = "";
+//     for (var i = 1; i <= totalPages; i++) {
+//         paginationHtml += 
+//         `<li class="page-item ${i == currentPage? "active" : ""}"><a class="page-link" href="#">${i}</a></li>`;
+//     }
+//     $("ul.pagination").html(paginationHtml);
+// }
+
 
